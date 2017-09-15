65095357ffc166e9aed44f68ac90fde0064dfa26
Tom Hynes <thomas.hynes@outlook.com>
2017-09-15 19:52:30 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -32,5 +32,5 @@ function removeElementFromBeginningOfArray(array) {
 return array.slice(1)
 }
 function destructivelyRemoveElementFromEndOfArray(array) {
-return array.pop(1)
+return array.pop()
 }
