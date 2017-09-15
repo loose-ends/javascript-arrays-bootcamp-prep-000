876409261d4cb8f0b1c61fdaf0a0782af86c28d2
Tom Hynes <thomas.hynes@outlook.com>
2017-09-15 19:29:22 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -6,7 +6,7 @@ var chocolateBars = [
 ]
 
 function addElementToBeginningOfArray(array, element) {
-[element, ...array]
+array=[element, ...array]
 return array
 }
 function destructivelyAddElementToBeginningOfArray(array, element) {
