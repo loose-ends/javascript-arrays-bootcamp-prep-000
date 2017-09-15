c3a429c1607c00a0c060beac93f63bfed927519f
Tom Hynes <thomas.hynes@outlook.com>
2017-09-15 18:22:59 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,16 +1,6 @@
-var grilledCheeseIngredients = [
-  'bread',
-  'mild cheese',
-  'sharp cheese',
-  'butter',
-  'tomato',
-  'garlic'
-]
- 
-var tomatoSauceIngredients = [
-  'tomato',
-  'garlic',
-  'olive oil',
-  'basil',
-  'oregano'
+var chocolateBars = [
+  'snickers',
+  'hundred grand',
+  'kitkat',
+  'skittles'
 ]
