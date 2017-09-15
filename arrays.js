81404f30b5555a0c72d9b1381923a09f4b1e26c1
Tom Hynes <thomas.hynes@outlook.com>
2017-09-15 19:49:01 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -28,3 +28,6 @@ function destructivelyRemoveElementFromBeginningOfArray(array, element) {
 array.shift(element)
 return array
 }
+function removeElementFromBeginningOfArray(array) {
+return array.slice(1)
+}
