87b79b3808b008da03b643c1dcb40eb71df2c5c0
Tom Hynes <thomas.hynes@outlook.com>
2017-09-15 19:14:48 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -12,7 +12,7 @@ function destructivelyAddElementToBeginningOfArray(array, element) {
 return array=array.unshift(element)
 }
 function addElementToEndOfArray(array, element) {
-return array=[array, ...element]
+return array=[array, ...(element)]
 }
 function destructivelyAddElementToEndOfArray(array, element) {
 return array.push(element)
