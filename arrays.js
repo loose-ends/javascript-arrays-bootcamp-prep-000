c7bc1c74289a2c5e6db804e299ee752eb335f256
Tom Hynes <thomas.hynes@outlook.com>
2017-09-15 20:03:44 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -14,7 +14,7 @@ array.unshift(element)
 return array
 }
 function addElementToEndOfArray(array, element) {
-array=[array, ...element]
+array=[...array, element]
 return array
 }
 function destructivelyAddElementToEndOfArray(array, element) {
